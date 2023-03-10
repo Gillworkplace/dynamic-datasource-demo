bfbcd12f37cc4e2cc920596d88dfb6849dcb5fad
912343041@qq.com <912343041@qq.com>
2023-03-10 18:37:51 +0800

    init

---
+++ src/test/java/com/gill/dynamic/datasource/DynamicDatasourceDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.gill.dynamic.datasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicDatasourceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
